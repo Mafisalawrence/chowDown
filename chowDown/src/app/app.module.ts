194129a6b5bbbff0982ea327cda529d63ea98937
lawrence <lmafisa@retrorabbit.co.za>
2019-02-15 10:38:19 +0200

    registration functionality and  file

--- chowDown/src/app/app.module.ts
+++ chowDown/src/app/app.module.ts
@@ -5,17 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AccountComponent } from './components/account/account.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+    {path: 'login', component: LoginComponent},
+    {path: 'registration', component: RegistrationComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    AccountComponent,
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ chowDown/src/app/components/registration/registration.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+
+  registrationValues = {
+    email : 'newtest@test.com',
+    password : '12345678'
+  };
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.doRegistration();
+  }
+
+  doRegistration() {
+    this.authService.doRegister(this.registrationValues);
+  }
+
+}
